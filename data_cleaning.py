5db725edf8a350242517ee57c75afc7d305fc1fc
jjiricek <jjiricek@mines.edu>
2025-04-23 13:52:19 -0600

    cleaned mate in ones for training

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,4 +4,6 @@ df = pd.read_csv('data/lichess_db_puzzle.csv')
 
 mate_in_one_df = df[df['Themes'].str.contains('mateIn1', case=False, na=False)]
 
-mate_in_one_df.to_csv('data/mate_in_one_puzzles.csv', index=False)
+cleaned_df = mate_in_one_df[['FEN', 'Moves']]
+
+cleaned_df.to_csv('data/cleaned_mate_in_one_puzzles.csv', index=False)
