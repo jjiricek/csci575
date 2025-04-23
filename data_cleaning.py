2d5b19970e33023e675ba1fb08346a804f82d1e1
tajernigan <tajernigan@mines.edu>
2025-04-23 13:38:51 -0600

    altered gitignore and added python script to get mate in ones

---
+++ data_cleaning.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv('data/lichess_db_puzzle.csv')
+
+mate_in_one_df = df[df['Themes'].str.contains('mateIn1', case=False, na=False)]
+
+mate_in_one_df.to_csv('data/mate_in_one_puzzles.csv', index=False)
